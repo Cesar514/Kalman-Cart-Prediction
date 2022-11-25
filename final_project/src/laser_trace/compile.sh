cf84479574879b01941ddccd7ff3a96f6c3bb46c
Cesar514 <63657223+Cesar514@users.noreply.github.com>
2022-11-25 19:38:44 +0000

    Delete final_project directory

--- final_project/src/laser_trace/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ----- Has to be run from the folder 'laser_trace'
-
-g++ laser_trace.cpp -fPIC -O3  -msse4 -shared -I/usr/include/python3.8 -lboost_python38 -lpython3.8 -o laser_trace.so
-
-mv laser_trace.so ../pf_localisation/
